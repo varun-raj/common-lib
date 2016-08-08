57c5ef69ad0f9a7cc386cd41ad77518b1a8f059d
Varun Raj <varun@skcript.com>
2016-08-08 15:16:35 +0530

    Added fewmore functions

--- index.js
+++ index.js
@@ -15,3 +15,43 @@ module.exports.humanFileSize = function(bytes,decimals) {
 	var i = Math.floor(Math.log(bytes) / Math.log(k));
 	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
+
+exports.makeTwoDigit = function(n){
+    return n > 9 ? "" + n: "0" + n;
+}
+
+
+exports.convertToIndexArray = function(hash_array) {
+	var numeric_array = [];
+	for ( var item in hash_array ){
+	    numeric_array.push( hash_array[ item ] );
+	}
+	return numeric_array;
+}
+
+
+exports.getURLParameter = function(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
+
+
+exports.getFirstLetter = function(s) {
+	return s.toUpperCase().charAt(0);
+}
+
+
+exports.formatebitrate = function(bits) {
+  if (typeof bits !== 'number') {
+    return '';
+  }
+  if (bits >= 1000000000) {
+    return (bits / 1000000000).toFixed(2) + ' Gbit/s';
+  }
+  if (bits >= 1000000) {
+    return (bits / 1000000).toFixed(2) + ' Mbit/s';
+  }
+  if (bits >= 1000) {
+    return (bits / 1000).toFixed(2) + ' kbit/s';
+  }
+  return bits.toFixed(2) + ' bit/s';
+}
